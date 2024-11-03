e28f3da43c8567bdcb009fd6139e45ed9ab8174a
Gabriel Mariani <gabymariani20.11@gmail.com>
2024-11-03 13:25:47 -0300

    Quitamos las variables 'let' dentro de los metodos

--- CalculadoraNecromerge.js
+++ CalculadoraNecromerge.js
@@ -14,12 +14,11 @@ class InvocadoresRango1 extends Invocadores {
         }
 
         exponente = nivel - 1
-        let niv = 20 * (2 ** exponente)
+        base = 2 ** exponente
+        nvRuna1 = 20 * (base)
 
-        if (runas >= niv) {
-            let base = 2 ** exponente
-            let cantidadDeInvocadores = runas / 20
-            let redondearCantidad = Math.floor(cantidadDeInvocadores)
+        if (runas >= nvRuna1) {
+            redondearCantidad = Math.trunc(cantidadDeInvocadores = runas / 20)
             console.log("\x1b[31m%s\x1b[0m", `${this.nombre}: Podes crear ${redondearCantidad} para alcanzar el nivel ${nivel}(es de base: ${base})`)
             if (cantidadDeInvocadores >= 32) {
                 console.log(`Podes crear un Legenday Minion`)
@@ -36,14 +35,13 @@ class InvocadoresRango2 extends Invocadores {
         }
 
         exponente = nivel - 1
-        let nivRuna1 = 20 * (2 ** exponente)
-        let nivRuna2 = 50 * (2 ** exponente)
-        let base = 2 ** exponente
+        base = 2 ** exponente
+        nvRuna1 = 20 * (base)
+        nvRuna2 = 50 * (base)
 
-        if (runa1 >= nivRuna1 && runa2 >= nivRuna2) {
-            let cantPorRunas1 = runa1 / 20
-            let cantPorRunas2 = runa2 / 50
-            let redondearCantidad
+        if (runa1 >= nvRuna1 && runa2 >= nvRuna2) {
+            cantPorRunas1 = runa1 / 20
+            cantPorRunas2 = runa2 / 50
             if (cantPorRunas1 < cantPorRunas2) {
                 redondearCantidad = Math.floor(cantPorRunas1)
             }
@@ -54,7 +52,7 @@ class InvocadoresRango2 extends Invocadores {
             if (redondearCantidad >= 32) {
                 console.log(`Podes crear un Legenday Minion`)
             }
-        } 
+        }
         else {
             this.calcularInvocador(runa1, runa2, nivel -= 1, exponente)
         }
@@ -67,13 +65,12 @@ class InvocadoresRango3 extends Invocadores {
         }
 
         exponente = nivel - 1
-        let nivRuna1 = 30 * (2 ** exponente)
-        let nivRuna2 = 30 * (2 ** exponente)
-        let base = 2 ** exponente
+        base = 2 ** exponente
+        nvRuna3 = 30 * (base)
+        nvRuna4 = 30 * (base)
 
-        if (runa1 >= nivRuna1 && runa2 >= nivRuna2) {
-            let cantidadDeInvocadores = runa1 / 30
-            let redondearCantidad = Math.trunc(cantidadDeInvocadores)
+        if (runa1 >= nvRuna3 && runa2 >= nvRuna4) {
+            redondearCantidad = Math.floor(cantidadDeInvocadores = runa1 / 30)
             console.log("\x1b[35m%s\x1b[0m", `${this.nombre}: Podes crear ${redondearCantidad} para alcanzar el nivel ${nivel}(es de base: ${base})`)
             if (cantidadDeInvocadores >= 32) {
                 console.log(`Podes crear un Legenday Minion`)
@@ -91,8 +88,19 @@ const lectern = new InvocadoresRango2("Lectern")
 const fridge = new InvocadoresRango2("Fridge")
 const portal = new InvocadoresRango3("Portal")
 
+// Variables
+let base
+let nvRuna1 //20*(2 ** exponente)
+let cantPorRunas1
+let nvRuna2 //50*(2 ** exponente)
+let cantPorRunas2
+let nvRuna3 //30*(2 ** exponente)
+let nvRuna4 //30*(2 ** exponente)
+let nivel = 5
+let cantidadDeInvocadores
+let redondearCantidad
 
 // grave.calcularInvocador(300, 5, null)
-supCupboard.calcularInvocador(979, 5, null)
-lectern.calcularInvocador(246, 300, 5, null)
-portal.calcularInvocador(57, 174, 5, null)
+supCupboard.calcularInvocador(979, nivel, null)
+lectern.calcularInvocador(246, 300, nivel, null)
+portal.calcularInvocador(57, 174, nivel, null)
